47e8db995a5e9a8f04afc33fc9f9c4c8d6481acd
Vinayak Prakash <vinayakprakash2121@gmail.com>
2025-06-05 15:45:00 +0530

    dd

--- kernal_reg.v
+++ kernal_reg.v
@@ -1,16 +1,26 @@
-module kerna_reg #(
+module kernal_reg #(
     parameter KERNEL_SIZE = 3,
-    parameter KERNEL_ADDR_WIDTH = 10,
+    parameter KERNEL_ADDR_WIDTH = 5,
     parameter WEIGHT_WIDTH = 8,
 
 ) (
     input i_clk,
     input i_rst,
-    input i_start,
-    input [KERNEL_ADDR_WIDTH-1:0] i_start_addr, // starting address of the kernel in BRAM0
-    input [KERNEL_ADDR_WIDTH-1:0] i_src1_start_addr, // starting address of the source data in BRAM1
+    input wr_en,
+    input [KERNEL_ADDR_WIDTH-1:0] wr_addr, // address to write to the kernel
+    input [KERNEL_ADDR_WIDTH-1:0] rd_addr, // address to read from the kernel
+    input [WEIGHT_WIDTH-1:0] wr_data, // data to write to the kernel
+    output reg [WEIGHT_WIDTH-1:0] rd_data, // data read from the kernel);
 );
+
 reg [7:0] kernal [KERNEL_SIZE*KERNEL_SIZE-1:0]; // Kernel buffer
 
+always @(posedge clk) begin
+    if(wr_en) begin
+        kernal[wr_addr] <= wr_data; // Write data to kernel
+    end
+end
+
+assign rd_data = kernal[rd_addr]; // Read data from kernel
 
 endmodule
