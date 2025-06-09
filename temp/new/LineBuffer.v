5b7fc1a05debff16cf4029d38ab0a1e0780ee6b2
Vinayak Prakash <vinayakprakash2121@gmail.com>
2025-06-09 14:45:23 +0530

    bvb

--- temp/new/LineBuffer.v
+++ temp/new/LineBuffer.v
@@ -7,22 +7,22 @@ module LineBuffer #(
     input [BIT_DEPTH-1:0] data_in_r1, // Data to write to LineBuffer
     input [BIT_DEPTH-1:0] data_in_r2, // Data to write to LineBuffer
     input [BIT_DEPTH-1:0] data_in_r3, // Data to write to LineBuffer
-    input [BIT_DEPTH-1:0] rd_data_r1, // to read from LineBuffer
-    input [BIT_DEPTH-1:0] rd_data_r2, // to read from LineBuffer
-    input [BIT_DEPTH-1:0] rd_data_r3, // to read from LineBuffer
+    output [BIT_DEPTH-1:0] rd_data_r1, // to read from LineBuffer
+    output [BIT_DEPTH-1:0] rd_data_r2, // to read from LineBuffer
+    output [BIT_DEPTH-1:0] rd_data_r3, // to read from LineBuffer
 );
 reg [BIT_DEPTH-1:0] buffer_row1 [2:0]; // LineBuffer memory array
 reg [BIT_DEPTH-1:0] buffer_row2 [2:0]; // LineBuffer memory array
 reg [BIT_DEPTH-1:0] buffer_row3 [2:0]; // LineBuffer memory array
 
 always @(posedge clk) begin
-    if (wr_en) begin
+    if (wr_en && ~shift) begin
         buffer_row1[0] <= data_in_r1; // Write data to LineBuffer row 1
         buffer_row2[0] <= data_in_r2; // Write data to LineBuffer row 2
         buffer_row3[0] <= data_in_r3; // Write data to LineBuffer row 3
     end
     // Shift the rows to the left
-    if(shift) begin
+    if(shift && ~wr_en) begin
         buffer_row1[1] <= buffer_row1[0];
         buffer_row1[2] <= buffer_row1[1];
         buffer_row2[1] <= buffer_row2[0];
