f782033fd83ab2737634a239752a7fa9562e7a5f
Vinayak Prakash <vinayakprakash2121@gmail.com>
2025-06-12 14:35:45 +0530

    d

--- temp/new/buffer.v
+++ temp/new/buffer.v
@@ -16,20 +16,19 @@ reg [7:0] buffer_row1 [0:COLS-1]; // LineBuffer memory array for row 1
 reg [7:0] buffer_row2 [0:COLS-1]; // LineBuffer memory array for row 2
 reg [7:0] buffer_row3 [0:COLS-1]; // LineBuffer memory array for row 3
 always @(posedge clk) begin
-    if (wr_en && ~shift) begin
+    if (wr_en || shift) begin
         buffer_row1[0] <= data_in_r1; // Write data to LineBuffer row 1
         buffer_row2[0] <= data_in_r2; // Write data to LineBuffer row 2
         buffer_row3[0] <= data_in_r3; // Write data to LineBuffer row 3
-    end
-    // Shift the rows to the left
-    if(shift && ~wr_en) begin
         integer i;
         for (i = 0; i < COLS-1; i = i + 1) begin
-            buffer_row1[i] <= buffer_row1[i+1];
-            buffer_row2[i] <= buffer_row2[i+1];
-            buffer_row3[i] <= buffer_row3[i+1];
+            buffer_row1[i+1] <= buffer_row1[i];
+            buffer_row2[i+1] <= buffer_row2[i];
+            buffer_row3[i+1] <= buffer_row3[i];
         end
     end
 end
-
+assign rd_data_r1 = buffer_row1[COLS-1]; // Read data from LineBuffer row 1
+assign rd_data_r2 = buffer_row2[COLS-1]; // Read data from LineBuffer row 2
+assign rd_data_r3 = buffer_row3[COLS-1]; // Read data from LineBuffer row 3
 endmodule
